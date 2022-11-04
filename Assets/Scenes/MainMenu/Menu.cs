39e50c6bf0da1653ab20ee1dadaed15d6f734a5d
Matthias <77961470+ImKogane@users.noreply.github.com>
2022-11-04 09:06:24 +0100

    Add, MainMenu

---
+++ Assets/Scenes/MainMenu/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEnter(InputValue Value)
+    {
+        Debug.Log("Changing to Lobby");
+        SceneManager.LoadScene("Assets/Scenes/Lobby/Lobby.unity");
+    }
+}
